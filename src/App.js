13dd719aaec18596578d21cd9680cb3ec28d03f1
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2021-09-27 19:44:24 +1000

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+
+import { CardList } from "./components/card-list/card-list.component";
+
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +10,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
   }
 
@@ -19,9 +23,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <h1>Monsters Rolodex</h1>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
